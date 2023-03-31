88c01a706e66c0c777609032d3e32f2b0ed2af91
Mary <maryandreeva100@gmail.com>
2023-04-01 01:58:44 +0300

    useState

---
+++ 05-react-green-button/script.js
@@ -0,0 +1,19 @@
+const App = () => {
+  const  [buttonText, setButtonText] = React.useState('Click me');
+  const [classesList, setClassesList] = React.useState('');
+
+  const onButtonClick = () => {
+    setButtonText('Hello from React');
+    setClassesList('green-btn');
+  };
+
+  return (
+    <div className="app">
+      <button className={classesList} onClick={onButtonClick}>{buttonText}</button>
+    </div>
+  )
+}
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
