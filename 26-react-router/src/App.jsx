5f47e64755cf12ea7be68d5ed51423d6a6161063
Mary <maryandreeva100@gmail.com>
2023-04-09 17:41:35 +0300

    Courses links

--- 26-react-router/src/App.jsx
+++ 26-react-router/src/App.jsx
@@ -3,8 +3,10 @@ import Home from './components/Home';
 import About from './components/About';
 import Contacts from './components/Contacts';
 import NotFound from './components/NotFound';
-import './App.css';
 import MainLayout from './layouts/MainLayout';
+import Courses from './components/Courses';
+import SingleCourse from './components/SingleCourse';
+import './App.css';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="contacts" element={<Contacts />} />
+            <Route path="courses" element={<Courses />} />
+            <Route path="courses/:courseSlug" element={<SingleCourse />} />
             <Route path="*" element={<NotFound />} />
             {/* для всех остальных путей */}
           </Route>
--- 26-react-router/src/components/Menu.jsx
+++ 26-react-router/src/components/Menu.jsx
@@ -4,6 +4,7 @@ const Menu = () => {
   return (
     <nav>
       <NavLink style={({ isActive }) => isActive ? {color: 'lightyellow', textDecoration: 'none'} : {}} to="." end>Home</NavLink>
+      <NavLink to="courses">Courses</NavLink>
       <NavLink className={({ isActive}) => isActive ? 'activeLink' : 'link' } to="about">About</NavLink>
       <NavLink className={({ isActive}) => isActive ? 'activeLink' : 'link' } to="contacts">Contacts</NavLink>
     </nav>
